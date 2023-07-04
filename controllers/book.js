45620668fcbc5a3283c13f19534bdf5f585671e1
Hasan <hasanjonsadullayev97@gmail.com>
2023-07-04 21:51:18 +0500

    pg connect

--- controllers/book.js
+++ controllers/book.js
@@ -1,43 +1,48 @@
-const { v4 } = require('uuid');
+const Book = require('../model/book');
 const fs = require('fs');
 const path = require('path');
 
-function getAll() {
-	return new Promise((resolve, reject) => {
-		fs.readFile(
-			path.join(__dirname, '..', 'db', 'data.json'),
-			'utf-8',
-			(err, data) => {
-				if (err) {
-					reject(err);
-				} else {
-					resolve(JSON.parse(data));
-				}
-			}
-		);
-	});
-}
-
 const addBook = async (req, res) => {
-	const books = await getAll();
-	books.push({ id: v4(), ...req.body }); // til: bok , auth: davlat
-
-	fs.writeFile(
-		path.join(__dirname, '..', 'db', 'data.json'),
-		JSON.stringify(books),
-		err => {
-			if (err) {
-				res.end('<h1>Kitob yuklanmadi</h1>');
-			} else {
-				res.redirect('/book');
-			}
-		}
-	);
+	const { title, author, page, price } = req.body;
+	const book = new Book(title, author, page, price);
+	await book.save();
+	res.redirect('/book');
 };
 
 const getAllBook = async (req, res) => {
-	const book = await getAll();
+	const book = await Book.getAll();
 	res.render('book', { title: 'Books', book });
 };
 
-module.exports = { addBook, getAllBook }; // {addbook: addbook, getAll: getAll}
+const getByIdBookforUpdate = async (req, res) => {
+	const books = await Book.getAll();
+	const data = books.find(book => book.id === req.body.id);
+	res.render('updateBook', { title: 'Update Book', data });
+};
+
+const updateBook = async (req, res) => {
+	try {
+		await Book.update(req.body);
+		res.redirect('/book');
+	} catch (error) {
+		console.log(error);
+		res.send('Book no updated');
+	}
+};
+
+const deleteBook = async (req, res) => {
+	try {
+		await Book.deleteBook(req.params.id);
+		res.redirect('/book');
+	} catch (error) {
+		res.send(`<h1>Ma'lumot o'chirilmadi</h1>`);
+	}
+};
+
+module.exports = {
+	addBook,
+	getAllBook,
+	getByIdBookforUpdate,
+	updateBook,
+	deleteBook,
+};
--- routes/book.js
+++ routes/book.js
@@ -1,69 +1,22 @@
 const { Router } = require('express');
 const route = Router();
-const fs = require('fs');
-const path = require('path');
 
-function getAll() {
-	return new Promise((resolve, reject) => {
-		fs.readFile(
-			path.join(__dirname, '..', 'db', 'data.json'),
-			'utf-8',
-			(err, data) => {
-				if (err) {
-					reject(err);
-				} else {
-					resolve(JSON.parse(data));
-				}
-			}
-		);
-	});
-}
-
-const { addBook, getAllBook } = require('../controllers/book');
+const {
+	addBook,
+	getAllBook,
+	getByIdBookforUpdate,
+	updateBook,
+	deleteBook,
+} = require('../controllers/book');
 
 route.post('/', addBook);
 
 route.get('/', getAllBook);
 
-route.post('/id', async (req, res) => {
-	const book = await getAll();
-	const data = book.find(book => book.id === req.body.id);
-	res.render('updateBook', { title: 'Update Book', data });
-});
+route.post('/id', getByIdBookforUpdate);
 
-route.post('/update', async (req, res) => {
-	const books = await getAll();
-	const data = books.filter(book => book.id !== req.body.id);
-	data.push(req.body);
+route.post('/update', updateBook);
 
-	fs.writeFile(
-		path.join(__dirname, '..', 'db', 'data.json'),
-		JSON.stringify(data),
-		err => {
-			if (err) {
-				res.end('<h1>Kitob yuklanmadi</h1>');
-			} else {
-				res.redirect('/book');
-			}
-		}
-	);
-});
-
-route.get('/delete/:id', async (req, res) => {
-	const books = await getAll();
-	const data = books.filter(book => book.id !== req.params.id);
-
-	fs.writeFile(
-		path.join(__dirname, '..', 'db', 'data.json'),
-		JSON.stringify(data),
-		err => {
-			if (err) {
-				res.end('<h1>Kitob yuklanmadi</h1>');
-			} else {
-				res.redirect('/book');
-			}
-		}
-	);
-});
+route.get('/delete/:id', deleteBook);
 
 module.exports = route;
